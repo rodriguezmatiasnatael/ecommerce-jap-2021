d8135451e47b919f930390375b9244993e0dcc87
rodriguezmatiasnatael <matiasnatael005@gmail.com>
2021-08-29 23:19:05 -0300

    entrega1
    Agregué a la página un inicio de sesión e hice una petición fetch para mostrar los productos en su respectiva pestaña

--- js/login.js
+++ js/login.js
@@ -2,5 +2,30 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    localStorage.setItem('logueado', 'false');
+    
+    function login(){
+        let loginButton = document.getElementById('loginButton');
+        loginButton.addEventListener('click', function(e){
+            e.preventDefault();
+            let $userName = document.getElementById('userName').value;
+            let $password = document.getElementById('password').value;
+            if($userName ==='' || $password ==='') {
+                alert('Ingrese usuario y contraseña');
+            } else if ($password.length < 9 && $userName.length < 6) {
+                alert('La contraseña debe contener por lo menos 9 caracteres y el usuario por lo menos 6');
+            } else if ($password.length >= 9 && $userName.length < 6){
+                alert('El nombre de usuario debe contener por lo menos 6 caracteres');
+            } else if ($password.length < 9 && $userName.length >= 6) {
+                alert('La contraseña debe contener por lo menos 9 caracteres');
+            } else {
+                localStorage.setItem('logueado', 'true');
+            }
+            if (localStorage.getItem('logueado') == 'true') {location.replace('index.html');}
+        });
+    }
+    
+    login();
 });
+
+
